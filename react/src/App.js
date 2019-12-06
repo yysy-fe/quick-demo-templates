b1211e44ba9fd9e57bcb997f3e13d6dfb2d2182e
yysy-fe <468068459@qq.com>
2019-12-06 11:27:16 +0800

    demo-init

---
+++ react/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import "./App.less"
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className="page-container">
+        react demo!1
+      </div>
+    );
+  }
+}
+
+export default App;
